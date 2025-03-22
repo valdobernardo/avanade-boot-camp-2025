a235c0d7c75a1dbb78cb9036dab68deb5c0051b4
valdo.junior <valdo.junior@DSK660BWP3>
2025-03-22 19:28:45 -0300

    Initial commit

---
+++ src/main/java/me/dio/AvanadeBootCamp2025Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanadeBootCamp2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanadeBootCamp2025Application.class, args);
+	}
+
+}
