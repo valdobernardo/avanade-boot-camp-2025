855dac6cfd8ea157eada1189b093cb7277892453
valdo.junior <valdo.junior@DSK660BWP3>
2025-03-23 00:56:21 -0300

    Implementação da camada de dominio

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AvanadeBootCamp2025Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AvanadeBootCamp2025Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
